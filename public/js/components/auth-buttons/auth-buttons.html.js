a707f99c684ace1d87f5ca7bfee922d4b3f9cdbe
Jonatan Wetterberg <jw223tb@student.lnu.se>
2025-05-22 13:35:00 +0200

    test 2

--- public/js/components/auth-buttons/auth-buttons.html.js
+++
@@ -1,6 +0,0 @@
-export const htmlTemplate = document.createElement('template')
-htmlTemplate.innerHTML = `
-  <div class="auth-buttons">
-    <div id="auth-actions"></div>
-  </div>
-`
--- public/js/components/auth-buttons/index.js
+++
@@ -1,1 +0,0 @@
-import './auth-buttons.js'
