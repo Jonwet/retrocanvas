5e7df171979c13858f897b61f6ee9cc32c23252b
Jonatan <jw223tb@student.lnu.se>
2025-05-25 14:35:17 +0200

    Removed test comment

--- src/server.js
+++ src/server.js
@@ -37,7 +37,7 @@ try {
   if (process.env.NODE_ENV === 'production') {
     app.set('trust proxy', 1)
   }
-  app.use(session(sessionOptions)) // TODO: Remove test comment
+  app.use(session(sessionOptions))
 
   app.use((req, res, next) => {
     res.locals.user = req.session.user || null
